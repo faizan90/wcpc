a20020ba766cda88183aa7bb698e50175c7ef6a1
Faizan_Home_6700K <faizananwar2006@gmail.com>
2018-11-24 21:29:51 +0100

    20181124
    backup

--- test_wcpc/snipping.py
+++ test_wcpc/snipping.py
@@ -16,19 +16,20 @@ if __name__ == '__main__':
 
     main_dir = Path(r'P:\Synchronize\IWS\2016_DFG_SPATE\data')
 
-#     in_net_cdf_file = main_dir / r'NCAR_ds010.0_18990101_20171030_dailydata.nc'
-#     out_net_cdf_file = main_dir / r'NCAR_ds010.0_19610101_20151231_dailydata_europe.nc'
+    in_net_cdf_file = main_dir / r'NCAR_ds010.0_18990101_20171030_dailydata.nc'
+    out_net_cdf_file = main_dir / r'NCAR_ds010.0_19800101_20091231_dailydata_germany.nc'
 
-    in_net_cdf_file = main_dir / r'ncep_1948_2017_level_500_6h.nc'
-    out_net_cdf_file = main_dir / r'ncep_1948_2017_level_500_6h_europe.nc'
+#     in_net_cdf_file = main_dir / r'ncep_1948_2017_level_500_6h.nc'
+#     out_net_cdf_file = main_dir / r'ncep_1948_2017_level_500_6h_europe.nc'
 
     os.chdir(main_dir)
 
     snip_nc = SnipNC()
     snip_nc.set_paths(in_net_cdf_file, out_net_cdf_file)
-    snip_nc.set_coords(330, 45, 35, 65, 'geo', 'lon', 'lat')
-#     snip_nc.set_times('1961-01-01', '2015-12-31', '%Y-%m-%d', [12, 13], 'H', 'time')
-    snip_nc.set_times('1948-01-01', '2017-12-31', '%Y-%m-%d', [0, 6, 12, 18], 'H', 'time')
+#     snip_nc.set_coords(330, 45, 35, 65, 'geo', 'lon', 'lat')
+    snip_nc.set_coords(2.0, 19, 40, 58, 'geo', 'lon', 'lat')
+    snip_nc.set_times('1980-01-01', '2009-12-31', '%Y-%m-%d', [12, 13], 'H', 'time')
+#     snip_nc.set_times('1948-01-01', '2017-12-31', '%Y-%m-%d', [0, 6, 12, 18], 'H', 'time')
     snip_nc.snip()
     snip_nc.save_snip()
 
